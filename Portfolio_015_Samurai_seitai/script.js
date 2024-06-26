c9ce868a4fad75b4258c3d5c9e2c09c79d858bc8
shinji-tsuboi <165543184+shinji-tsuboi@users.noreply.github.com>
2024-06-26 18:43:37 +0900

    portfolio_015をアップロード

---
+++ Portfolio_015_Samurai_seitai/script.js
@@ -0,0 +1,20 @@
+
+$(function() {
+  $('.top-carousel').slick({
+    autoplay: true,
+    fade: true,
+    dots: false,
+    aroows: false,
+    infinite: true,
+  });
+});
+
+$(function() {
+  $('.bottom-carousel').slick({
+    autoplay: true,
+    fade: true,
+    dots: false,
+    aroows: false,
+    infinite: true,
+  });
+});
