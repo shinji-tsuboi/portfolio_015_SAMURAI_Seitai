2b82b85aebd9c10f64b740688d516092a1768123
shinji tsuboi <shinjitsuboi7@gmail.com>
2024-07-02 19:38:31 +0900

    top-htmlを変更

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 $(function() {
   $('.top-carousel').slick({
     autoplay: true,
+    autoplaySpeed: 5000,
+    speed: 1500,
     fade: true,
     dots: false,
     aroows: false,
@@ -12,9 +14,20 @@ $(function() {
 $(function() {
   $('.bottom-carousel').slick({
     autoplay: true,
+    autoplaySpeed: 5000,
+    speed: 1500,
     fade: true,
     dots: false,
     aroows: false,
     infinite: true,
   });
+});
+
+// 100pxを境にTOPに戻るボタンの表示・非表示を切り替える
+$(window).scroll(function () {
+  if ($(this).scrollTop() > 100) {
+    $('#page-top').fadeIn();
+  } else {
+    $('#page-top').fadeOut();
+  }
 });
